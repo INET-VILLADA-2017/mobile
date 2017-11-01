0cd85466042c662af5c9f854b8620ce9f39c3850
Unknown <franpitri@gmail.com>
2017-11-01 12:42:18 -0300

    add actions & reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux'
 import authReducer from './auth'
 import navReducer from './nav'
+import dataReducer from './data'
 
 const AppReducer = combineReducers({
     auth: authReducer,
     nav: navReducer,
+    data: dataReducer
 });
 
 export default AppReducer;
